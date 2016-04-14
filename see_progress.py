3ae1cc6bf034e1a96acd84bb3cc07ae2c7d5d511
asakurayasunobu <tennisabc562@gmail.com>
2016-04-14 22:39:40 +0900

    first

--- see_progress.py
+++ see_progress.py
@@ -7,7 +7,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from collections import OrderedDict
 
-
 def getDirs(path):
     dirs = list()
     for item in os.listdir(path):
@@ -19,12 +18,12 @@ def checkFileNum(path, counter):
     sep = '/'
     dirs = os.listdir(path)
     for d in dirs:
-        if os.path.isdir(path + d):
-            counter = checkFileNum(path + d + sep, counter)
+        if os.path.isdir(path + sep +  d):
+            counter = checkFileNum(path + sep + d, counter)
         else:
-            counter += 1
+            if '.py' in d:
+                counter += 1
     return counter
-    #print("{}files".format(counter))
 
 def showProgress(progress):
     X = range(len(progress.keys()))
