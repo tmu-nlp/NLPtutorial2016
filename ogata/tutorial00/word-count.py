62a425f64e3727812add1bac454280745e0069c7
tomoyao <silent00.tomo@gmail.com>
2016-04-19 18:31:47 +0900

    Update word-count.py

--- ogata/tutorial00/word-count.py
+++ ogata/tutorial00/word-count.py
@@ -3,14 +3,14 @@ f_in = open(sys.argv[1], "r")
 lines = f_in.readlines()
 f_in.close()
 words = "".join(lines).split()
-s = dict()
+count = dict()
 for word in words:
-  if word in s.keys():
-    s[word] += 1
+  if word in count.keys():
+    count[word] += 1
   else:
-    s.update([(word, 1)])
+    count.update([(word, 1)])
 f_out = open("answer.txt", "w")
-for k, v in s.iteritems():
+for k, v in count.iteritems():
   f_out.write("{} {}\n".format(k, v))
 f_out.close()
   
