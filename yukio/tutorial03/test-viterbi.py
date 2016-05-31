01c53d0a899ab3f363974d9414865ada7f449582
Yukio Matsumura <Yukio@Yukio-no-MacBook-Air.local>
2016-05-31 14:51:09 +0900

    added tutuorial03

---
+++ yukio/tutorial03/test-viterbi.py
@@ -0,0 +1,35 @@
+import math
+from collections import defaultdict
+
+unigram_probabilities = defaultdict(lambda: 0)
+L1 = 0.95
+V = 1000000
+
+for line in open("model.txt", "r"):
+    words = line.split()
+    if len(words) == 2:
+        unigram_probabilities[words[0]] = float(words[1])
+
+for line in open ("../../data/wiki-ja-test.word", "r"):
+    line = line.strip("\t")
+    best_edge = [None]
+    best_score = [0]
+    for word_end in range(1, len(line) + 1):
+        best_score.append(10 ** 10)
+        for word_begin in range(0, word_end):
+            word = line[word_begin:word_end]
+            if word in unigram_probabilities or len(word) == 1:
+                prob = L1 * unigram_probabilities[word] + (1 - L1) / V
+                my_score = best_score[word_begin] - math.log(prob, 2)
+                if my_score < best_score[word_end]:
+                    best_score[word_end] = my_score
+                    best_edge.append((word_begin, word_end))
+
+    words = []
+    next_edge = best_edge[len(best_edge) - 1]
+    while next_edge != None:
+        word = line[next_edge[0]:next_edge[1]]
+        words.append(word)
+        next_edge = best_edge[next_edge[0]]
+    words.reverse()
+    print(" ".join(words))
