b5454a430e6fe10cf5b84d2074358efe631a0c15
尾形朋哉 <tomoya@tmy.local>
2016-05-31 16:56:33 +0900

    upload

--- ogata/tutorial01/train-unigram.py
+++ ogata/tutorial01/train-unigram.py
@@ -1,15 +1,27 @@
 import sys
 from collections import defaultdict
-counts = defaultdict(int)
-total_count = 0
-for line in open("wiki-en-train.word"):
-  words = line.split()
-  words.append("<\s>")
-  for word in words:
-    counts[word] += 1
-    total_count += 1
 
-model_file = open("model_file.txt", "w")
-for word, count in counts.items():
-  probability = counts[word]/total_count
-  model_file.write("{}\t{}\n".format(word, probability))
+def getUnigramProb():
+  counts = defaultdict(int)
+  probDict = defaultdict(int)
+  total_count = 0
+  for line in open("wiki-en-train.word"):
+    words = line.split()
+    words.append("<\s>")
+    for word in words:
+      counts[word] += 1
+      total_count += 1
+
+  for word, count in counts.items():
+    probability = counts[word]/total_count
+    probDict[word] = probability
+  return probDict
+
+if __name__ == "__main__":
+  probDict = getUnigramProb()
+  model_file = open("model_file.txt", "w")
+  for word, probability in sorted(probDict.items()):
+    model_file.write("{}\t{}\n".format(word, probability))
+  model_file.close()
+
+    
