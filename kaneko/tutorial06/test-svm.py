c9bbc7a1b61637f960580fa8a8381094ec241e61
正弘金子 <masahirokaneko@masahirokaneko-no-MacBook-Air.local>
2016-06-28 16:33:56 +0900

    aaa

---
+++ kaneko/tutorial06/test-svm.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+from train_svm import *
+
+
+def preditct_all(model_file, input_file):
+    w = defaultdict(int)
+    for line in open(model_file):
+        key, value = line.split()
+        w[key] = float(value)
+    for x in open(input_file):
+        phi= create_features(x)
+        yy = predict_one(w, phi)
+        print(yy)
+
+
+if __name__ == "__main__":
+    preditct_all("perceptron_model.txt","titles-en-test.word")
